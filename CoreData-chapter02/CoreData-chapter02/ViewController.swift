c08197416089168d2221c04805e7e3d961d85761
haobo <haobo.yang@1hai.cn>
2021-02-18 08:21:10 +0800

    no message

--- CoreData-chapter02/CoreData-chapter02/ViewController.swift
+++ CoreData-chapter02/CoreData-chapter02/ViewController.swift
@@ -9,11 +9,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let semgentControl : UISegmentedControl = {
+        let semgentControl = UISegmentedControl(frame: CGRect(x: 10, y: 10, width: UIScreen.main.bounds.width, height: 30))
+        semgentControl.target(forAction: Selector(("semengentAction")), withSender: self)
+        
+        return semgentControl
+    }()
+    
+    private let nameLabel : UILabel = {
+        let lab = UILabel()
+        lab.numberOfLines = 1
+        lab.textAlignment = .left
+        lab.textColor = UIColor.black
+        lab.backgroundColor = UIColor.clear
+        return lab
+    }()
+    
+    private let ratingLabel : UILabel = {
+        let lab = UILabel()
+        lab.numberOfLines = 1
+        lab.textAlignment = .left
+        lab.textColor = UIColor.black
+        lab.backgroundColor = UIColor.clear
+        return lab
+    }()
+    
+    private let timesWornLabel : UILabel = {
+        let lab = UILabel()
+        lab.numberOfLines = 1
+        lab.textAlignment = .left
+        lab.textColor = UIColor.black
+        lab.backgroundColor = UIColor.clear
+        return lab
+    }()
+    
+    private let lastWornLabel : UILabel = {
+        let lab = UILabel()
+        lab.numberOfLines = 1
+        lab.textAlignment = .left
+        lab.textColor = UIColor.black
+        lab.backgroundColor = UIColor.clear
+        return lab
+    }()
+    
+    private let favoriteLabel : UILabel = {
+        let lab = UILabel()
+        lab.numberOfLines = 1
+        lab.textAlignment = .left
+        lab.textColor = UIColor.black
+        lab.backgroundColor = UIColor.clear
+        return lab
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(self.semgentControl)
+        
     }
-
+    func semengentAction () {
+        
+    }
 
 }
 
