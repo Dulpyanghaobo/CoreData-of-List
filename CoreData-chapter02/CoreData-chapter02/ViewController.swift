cd454c7cf16eb3ab32e9b276d2104cb33d6b9654
haobo <haobo.yang@1hai.cn>
2021-02-18 08:20:58 +0800

    no message

---
+++ CoreData-chapter02/CoreData-chapter02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData-chapter02
+//
+//  Created by yhb on 2021/2/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
